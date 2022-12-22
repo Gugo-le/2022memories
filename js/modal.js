4dda54919827ba31e1aaa005bf8d553da5447764
Gugo-le <guhyunseung0718@gmail.com>
2022-12-22 16:24:22 +0900

    update

--- js/modal.js
+++ js/modal.js
@@ -10,6 +10,19 @@ btnOpenPopup.addEventListener('click', () => {
     if (modal.classList.contains('show')) {
         body.style.overflow = 'hidden';
     }
+
+    if (modal.classList.contains('show')) {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            // Not adding `{ audio: true }` since we only want video now
+            navigator.mediaDevices.getUserMedia({
+                video: true
+            }).then(function(stream) {
+                //video.src = window.URL.createObjectURL(stream);
+                video.srcObject = stream;
+                video.play();
+            });
+        }
+    }
 });
 
 modal.addEventListener('click', (event) => {
