cde5da2133bef7a2375815546909d5a9876373d9
Gugo-le <rngustmd0719@naver.com>
2022-12-23 01:10:24 +0900

    upload

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const mysql = require('mysql');
+const multer = require('multer');
+const fs = require('fs');
 const app = express();
+var router = express.Router();
+module.exports = router;
 
-app.get('/', function(요청, 응답) {
-    응답.send('여기는 나의 작업용 페이지입니다.');
+app.use(express.json());
+console.log(__dirname + '/uploads');
+app.use(express.static(__dirname + '/uploads'));
+
+app.listen(8080, () => {
+    console.log("서버시작, 8080번 포트");
 });
 
-app.listen(8000, function() {
-    console.log('Listening at 8000');
+let storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, "uploads/");
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+
+let upload = multer({
+    storage: storage
+});
+
+app.post('/api/uploadimage', upload.single("imgfile"), (req, res, next) => {
+    let file = req.file;
+
+    console.log("접속함");
+
+    res.json({
+        success: true,
+        imageurl: `http://127.0.0.1:8080/uploads/'${req.filename}'`
+    });
 });
