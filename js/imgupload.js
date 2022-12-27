df7489a275099d9f1221993c908245a314bd244a
_G.U.G.O_ <85179784+Gugo-le@users.noreply.github.com>
2022-12-27 14:52:56 +0900

    Update imgupload.js

--- js/imgupload.js
+++ js/imgupload.js
@@ -112,5 +112,21 @@ function imgon() {
 
 function imgoff() {
     document.getElementById("canvas").style.display = "none";
-
 }
+
+$(document).ready(function() {
+    $('.clipboardBtn').on('click', function(e) {
+
+        var text = 'https://2022memories.vercel.app';
+        $('#clip_target').val(text);
+        $('#clip_target').select();
+        // Use try & catch for unsupported browser 
+        try {
+            // The important part (copy selected text) 
+            var successful = document.execCommand('copy');
+            // if(successful) answer.innerHTML = 'Copied!'; 
+            // else answer.innerHTML = 'Unable to copy!';
+        } catch (err) { alert('이 브라우저는 지원하지 않습니다.') }
+
+    });
+});
